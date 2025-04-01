aa0d96dc7cdc63e1f4cdd27a52924ec6c4490a8f
Prayas911 <prayasraghav123@gmail.com>
2025-04-01 10:12:12 +0530

    Add files via upload

---
+++ backend/day1.js
@@ -0,0 +1,32 @@
+//SYNC FILE SYSTEM
+
+// const fs= require('fs')
+// console.log("1")
+// const data=fs.readFileSync("day1.txt", "utf8");
+// console.log(data)
+// console.log("2")============================================= EXAMPLE OF SYNC FILE SYSTEM===>(READ MODE)
+
+
+
+
+//  const fs= require('fs')
+//  fs.writeFileSync("elce.txt","i am a student");==================EXAMPLE OF SYNC FILE SYSTEM===>(WRITE MODE MODE)
+
+ 
+
+//ASYNC FILE SYSTEM : READ MODE
+
+// const fs= require('fs');
+// const data=fs.readFile("day1.txt","utf8",(err,data)=>{          //(err,data)==>callback function
+//     console.log(data)
+
+// });
+
+
+//ASYNC FILE SYSTEM : WRITE MODE
+
+// const fs= require('fs');
+// const data=fs.writeFile("elc.txt","prayas raghav",(err,data)=>{          //(err,data)==>callback function
+// console.log(data)
+
+// });
